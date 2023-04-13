ae544f948705a86e362dbfc6123fc6c449b1147d
NilsHMeier <66554930+NilsHMeier@users.noreply.github.com>
2023-04-13 10:13:19 +0200

    Add first exercise - Update readme.md - Update .gitignore

---
+++ exercise_01.py
@@ -0,0 +1,44 @@
+import torch as th
+
+
+def task1():
+    # Start with a 13x13 matrix of ones
+    matrix = th.full(size=(13, 13), fill_value=1, dtype=th.int8)
+
+    # Fill in the columns and rows with twos
+    matrix[[1, 6, -1], :] = 2
+    matrix[:, [1, 6, -1]] = 2
+
+    # Fill in the left threes
+    matrix[3:5, 3:5] = 3
+    matrix[-5:-3, 3:5] = 3
+
+    # Fill in the right threes
+    matrix[3:5, -5:-3] = 3
+    matrix[-5:-3, -5:-3] = 3
+
+    # Print out the matrix
+    print(matrix)
+
+
+def sigmoid(x):
+    return 1.0 / (1.0 + th.exp(-x))
+
+
+def softmax(x):
+    return th.exp(x) / th.sum(th.exp(x))
+
+
+def task3():
+    # Generate a random vector
+    random_vector = th.randint(low=-5, high=5, size=(5, ), dtype=th.float32)
+
+    # Compare the outputs
+    print(f'Inputs: {random_vector}')
+    print(f'Sigmoid: {sigmoid(random_vector)}')
+    print(f'Softmax: {softmax(random_vector)}')
+
+
+if __name__ == '__main__':
+    task1()
+    task3()
--- readme.md
+++ readme.md
@@ -3,10 +3,10 @@ This repository contains some of the solutions to the tasks in the Deep Learning
 Samuel Gomes Fadel at the Leuphana University of Lüneburg in the summer semester 2023. The solutions are not necessarily
 correct and may differ from the solutions given by the lecturer. The solutions are provided as is and without any 
 warranty. If you find any errors or have any suggestions, please feel free to open an issue or a pull request. The
-content in this repository are updateded regularly based on the progress of the course.
+content in this repository are updated regularly based on the progress of the course.
 
 ## Lecture Contents
 - Lecture 1: Motivation for Deep Learning & Intro to Perceptron
 
 ## Exercises
-No exercises yet.
+- Exercise 1: PyTorch basics, Sigmoid and Softmax
